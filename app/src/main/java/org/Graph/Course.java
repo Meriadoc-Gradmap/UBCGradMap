07774634ba0618c47f511e59577661caea985a50
BenUBC <ben5@student.ubc.ca>
2024-11-02 22:16:17 -0700

    Course getters and setters

--- app/src/main/java/org/Graph/Course.java
+++ app/src/main/java/org/Graph/Course.java
@@ -3,6 +3,11 @@ package org.Graph;
 import java.util.Set;
 import java.util.HashSet;
 
+/**
+ * Course class.
+ *
+ * @author Ben Newington
+ */
 public class Course extends Vertex {
 
     private String courseCode;
@@ -14,6 +19,10 @@ public class Course extends Vertex {
     private Set<String> preRequisites;
     private Set<String> dependants;
 
+    /**
+     * Creates a course.
+     * @param id must be positive.
+     */
     public Course(int id) {
         super(id);
     }
@@ -23,13 +32,98 @@ public class Course extends Vertex {
     }
 
     public Set<String> getPreRequisites() {
-        return preRequisites;
+        return new HashSet<>(preRequisites);
     }
 
     public Set<String> getDependants() {
-        return dependants;
+        return new HashSet<>(dependants);
     }
 
-    // TODO: create getters and setters for all field
+    public String getDescription() {
+        return description;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public Hours getWeeklyHours() {
+        return weeklyHours;
+    }
+
+    /* Setters */
+
+    public boolean setCourseCode(String code) {
+        if (courseCode == null || courseCode.isEmpty()) {
+            this.courseCode = code;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setPreRequisites(Set<String> requisites) {
+        if (preRequisites == null || preRequisites.isEmpty()) {
+            this.preRequisites = new HashSet<>(requisites);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setDependants(Set<String> dependantSet) {
+        if (dependants == null || dependants.isEmpty()) {
+            this.dependants = new HashSet<>(dependantSet);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setDescription(String descriptionString) {
+        if (description == null || description.isEmpty()) {
+            this.description = descriptionString;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setName(String nameString) {
+        if (name == null || name.isEmpty()) {
+            this.name = nameString;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setCredits(int creditsInt) {
+        if (credits == 0) {
+            this.credits = creditsInt;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setAverage(double averageDouble) {
+        if (average == 0) {
+            this.average = averageDouble;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setWeeklyHours(Hours weeklyHoursRecord) {
+        if (weeklyHours == null) {
+            this.weeklyHours = new Hours(weeklyHoursRecord.lectures(), weeklyHoursRecord.alternating1(),
+                    weeklyHoursRecord.labs(), weeklyHoursRecord.alternating2(),
+                    weeklyHoursRecord.tutorials(), weeklyHoursRecord.alternating3());
+            return true;
+        }
+        return false;
+    }
 }
