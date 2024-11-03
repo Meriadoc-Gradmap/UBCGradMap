c3f07b29d97f1ccf5d8035c135cbccbed68141bd
BenUBC <ben5@student.ubc.ca>
2024-11-03 13:14:06 -0800

    Json to class
    Co-Authored-By: Iain05 <58150364+Iain05@users.noreply.github.com>

--- app/src/main/java/org/Graph/GraphCreator.java
+++ app/src/main/java/org/Graph/GraphCreator.java
@@ -1,8 +1,34 @@
 package org.Graph;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class GraphCreator {
-    Gson gson = new Gson();
+
+    public GraphCreator() {
+        Gson gson = new Gson();
+        try {
+            JsonReader reader = new JsonReader(new FileReader("data/Example.json"));
+            Type listType = new TypeToken<List<Course>>() {}.getType();
+            List<Course> courses = gson.fromJson(reader, listType);
+            for (Course course : courses) {
+                course.initId();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphCreator gc = new GraphCreator();
+    }
+
 }
