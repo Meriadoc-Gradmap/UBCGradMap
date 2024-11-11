23596dbc7f57fcfc0e609f52e75402bfb7296fe7
BenUBC <ben5@student.ubc.ca>
2024-11-11 09:36:52 -0800

    Specs

--- app/src/main/java/org/Graph/Course.java
+++ app/src/main/java/org/Graph/Course.java
@@ -10,19 +10,28 @@ import java.util.HashSet;
  */
 public class Course extends Vertex {
 
-    private String code = "";
-    private String name = "";
-    private int[] credits = null;
-    private String description = "";
-    private Set<String> prerequisites = null;
-    private Set<String> postrequisites = null;
-    private boolean cdf = false;
-    private Hours schedule = null;
-    private Others others = null;
+    private final String code;
+    private final String name;
+    private final int[] credits;
+    private final String description;
+    private final Set<String> prerequisites;
+    private final Set<String> postrequisites;
+    private final boolean cdf;
+    private final Hours schedule;
+    private final Others others;
     private static int id = 0;
 
     /**
      * Creates a course.
+     * @param code course code "XXXX###"
+     * @param name course name
+     * @param credits credits for each version of the course
+     * @param description description
+     * @param prerequisites list of course codes that are prerequisites
+     * @param postrequisites list of course codes that are dependents
+     * @param cdf if the course is credit d fail
+     * @param schedule record of how many hours per week the course is
+     * @param othersRecord other information
      */
     public Course(String code, String name, int[] credits, String description,
                   Set<String> prerequisites, Set<String> postrequisites, boolean cdf,
@@ -41,47 +50,90 @@ public class Course extends Vertex {
         this.cdf = cdf;
     }
 
+    /**
+     * Sets the id to the previous course's id plus one.
+     */
     public void initId() {
         super.setId(id);
         id++;
     }
 
+    /**
+     * Gets the course code.
+     * @return course code
+     */
     public String getCourseCode() {
         return code;
     }
 
+    /**
+     * Gets the pre-requisites.
+     * @return a set of pre-requisites
+     */
     public Set<String> getPreRequisites() {
         return new HashSet<>(prerequisites);
     }
 
+    /**
+     * Gets the post-requisites.
+     * @return a set of post-requisites
+     */
     public Set<String> getPostRequisites() {
         return new HashSet<>(postrequisites);
     }
 
+    /**
+     * Gets the description.
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Gets the name.
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the number of credits for each course version.
+     * @return an array of the number of credits
+     */
     public int[] getCredits() {
         return credits;
     }
 
+    /**
+     * Gets the average.
+     * @return the average
+     */
     public double getAverage() {
         return others.average();
     }
 
+    /**
+     * Gets the weekly hours.
+     * @return the weekly hours
+     */
     public Hours getWeeklyHours() {
         return schedule.copy();
     }
 
+    /**
+     * Gets the other information.
+     * @return other information
+     */
     public Others getOthers() {
         return others.copy();
     }
 
+    /**
+     * Returns <code>true</code> if the course is a credit/d/fail.
+     * @return <code>true</code> if the course is a credit/d/fail, <code>false</code> otherwise
+     */
     public boolean isCdf() {
         return cdf;
     }
