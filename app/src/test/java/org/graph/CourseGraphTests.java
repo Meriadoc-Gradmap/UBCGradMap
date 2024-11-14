5d2ff5871b387e9f3d1d26cb01f0a977310181b8
William Banquier <54950146+William-Banquier@users.noreply.github.com>
2024-11-13 18:04:30 -0800

    finished making tests for graph -- probably will need some modifications -- a strong portion of them fail

--- app/src/test/java/org/graph/CourseGraphTests.java
+++ app/src/test/java/org/graph/CourseGraphTests.java
@@ -1,4 +1,159 @@
 package org.graph;
 
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CourseGraphTests {
+
+    int[] acred;
+    int[] bcred;
+    int[] ccred;
+
+    Set<String> aPreq;
+    Set<String> aPost;
+    Set<String> bPreq;
+    Set<String> bPost;
+    Set<String> cPreq;
+    Set<String> cPost;
+
+    Hours aHours;
+    Hours bHours;
+    Hours cHours;
+
+    Others aOther;
+    Others bOther;
+    Others cOther;
+
+    Course a;
+    Course b;
+    Course c;
+
+    Set<Course> courseSet;
+
+    @BeforeEach
+    public void beforeEach(){
+        // create three courses that link together
+        // a and b are coreqs
+        // together you can do c
+        // a + b = c :-)
+
+        acred = new int[] {1,2};
+        bcred = new int[] {3};
+        ccred = new int[] {112}; // yep it's a lot of credits
+
+        aPreq = new HashSet<>();
+        bPreq = new HashSet<>();
+        aPost = new HashSet<>();
+        bPost = new HashSet<>();
+        aPost.add("c");
+        bPost.add("c");
+
+        cPreq = new HashSet<>();
+        cPreq.add("a"); cPreq.add("b");
+        cPost = new HashSet<>();
+
+        aHours = new Hours(1,false, 1,false,1,false);
+        bHours = new Hours(1,false, 1,false,1,false);
+        cHours = new Hours(1,false, 1,false,1,false);
+
+        aOther = new Others(0,"Dr. Prof");
+        bOther = new Others(50,"Dr. Prof");
+        cOther = new Others(10,"Dr. Prof");
+
+        a = new Course(
+                "a",
+                "course a",
+                acred,
+                "desc a",
+                aPreq,
+                aPost,
+                false,
+                aHours,
+                aOther);
+        b = new Course(
+                "b",
+                "course b",
+                bcred,
+                "desc b",
+                bPreq,
+                bPost,
+                false,
+                bHours,
+                bOther);
+        c = new Course(
+                "c",
+                "course c",
+                ccred,
+                "desc c",
+                cPreq,
+                cPost,
+                false,
+                cHours,
+                cOther);
+
+        courseSet = new HashSet<>();
+        courseSet.add(a);
+        courseSet.add(b);
+        courseSet.add(c);
+    }
+
+    @Test
+    public void invalidCourseGraph(){
+        // will it notice if id < 0 or if id > graph size
+        while (a.id() != -42) // not the meaning of life
+            a.initId();
+        assertThrows(IllegalArgumentException.class, () -> new CourseGraph(courseSet));
+
+        while (a.id() != 42)
+            a.initId();
+        assertThrows(IllegalArgumentException.class, () -> new CourseGraph(courseSet));
+    }
+
+    @Test
+    public void testCoReqs(){
+        CourseGraph cg = new CourseGraph(courseSet);
+        Set<String> coreqsofA = new HashSet<>();
+        coreqsofA.add("a");
+        coreqsofA.add("b");
+        assertEquals(coreqsofA, cg.getCoRequisites("a"));
+        assertEquals(coreqsofA, cg.getCoRequisites("b"));
+    }
+
+    @Test
+    public void getNames(){
+        CourseGraph cg = new CourseGraph(courseSet);
+        // these are the codes, not the names fyi
+        String[] expectedNames = new String[] {"a","b","c"};
+        String[] actualNames = cg.getNames();
+
+        assertEquals(new HashSet<>(List.of(expectedNames)), new HashSet<>(List.of(actualNames)));
+    }
+
+    @Test
+    public void testGettingPreAndPost(){
+        CourseGraph cg = new CourseGraph(courseSet);
+        assertEquals(aPreq, cg.getPreRequisites("a"));
+        assertEquals(bPreq, cg.getPreRequisites("b"));
+        assertEquals(cPreq, cg.getPreRequisites("c"));
+
+        assertEquals(aPost, cg.getPostRequisites("a"));
+        assertEquals(bPost, cg.getPostRequisites("b"));
+        assertEquals(cPost, cg.getPostRequisites("c"));
+    }
+
+    @Test
+    public void getCourseTest(){
+        CourseGraph cg = new CourseGraph(courseSet);
+        assertEquals(a, cg.getCourse("a"));
+        assertEquals(b, cg.getCourse("b"));
+        assertEquals(c, cg.getCourse("c"));
+    }
 }
--- app/src/test/java/org/graph/CourseTests.java
+++ app/src/test/java/org/graph/CourseTests.java
@@ -1,4 +1,240 @@
 package org.graph;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CourseTests {
+
+
+    @Test
+    public void createValidCourse(){
+
+        Set<String> prereqs = new HashSet<>();
+        prereqs.add("pre1");
+        prereqs.add("pre2");
+
+        Set<String> postreqs = new HashSet<>();
+        postreqs.add("post1");
+        postreqs.add("post2");
+
+        Course c = new Course(
+                "code",
+                "name",
+                new int[]{0},
+                "desc",
+                prereqs,
+                postreqs,
+                false,
+                new Hours(1, false, 1, false, 1, false),
+                new Others(12, "dr. prof")
+        );
+
+        assertEquals("code", c.getCourseCode());
+        assertEquals("name", c.getName());
+        assertEquals(0, c.getCredits()[0]);
+        assertEquals(1, c.getCredits().length);
+        assertEquals("desc", c.getDescription());
+
+        assertEquals(prereqs, c.getPreRequisites());
+        assertNotSame(prereqs, c.getPreRequisites());
+        // make sure it is a separate object
+        assertEquals(postreqs, c.getPostRequisites());
+        assertNotSame(postreqs, c.getPostRequisites());
+
+        assertFalse(c.isCdf());
+        assertEquals(new Hours(1, false, 1, false, 1, false), c.getWeeklyHours());
+        assertEquals(new Others(12, "dr. prof"), c.getOthers());
+
+        assertEquals(12, c.getAverage());
+    }
+
+    @Test
+    public void overflowid(){
+
+        Set<String> prereqs = new HashSet<>();
+        prereqs.add("pre1");
+        prereqs.add("pre2");
+
+        Set<String> postreqs = new HashSet<>();
+        postreqs.add("post1");
+        postreqs.add("post2");
+
+        Course c = new Course(
+                "code",
+                "name",
+                new int[]{0},
+                "desc",
+                prereqs,
+                postreqs,
+                false,
+                new Hours(1, false, 1, false, 1, false),
+                new Others(12, "dr. prof")
+        );
+
+        while (c.id() != Integer.MAX_VALUE)
+        {c.initId();}
+
+        // we are making the id negative which is not allowed in the vertex class
+        assertThrows(IllegalArgumentException.class, c::initId);
+        System.out.println(c.id()); // it is integer.min_valid fyi
+
+        // we are making a class with an negative id this should fail as well
+        assertThrows(IllegalArgumentException.class , ()->
+                new Course(
+                        "code",
+                        "name",
+                        new int[]{0},
+                        "desc",
+                        prereqs,
+                        postreqs,
+                        false,
+                        new Hours(1, false, 1, false, 1, false),
+                        new Others(12, "dr. prof")
+                ));
+    }
+
+    @Test
+    public void fakeEquals(){
+        Set<String> prereqs = new HashSet<>();
+        prereqs.add("pre1");
+        prereqs.add("pre2");
+
+        Set<String> postreqs = new HashSet<>();
+        postreqs.add("post1");
+        postreqs.add("post2");
+
+        Course a = new Course(
+                "code",
+                "name",
+                new int[]{0},
+                "desc",
+                prereqs,
+                postreqs,
+                false,
+                new Hours(1, false, 1, false, 1, false),
+                new Others(12, "dr. prof")
+        );
+        Course b = new Course(
+                "NOT THE SAME COURSE",
+                "name",
+                new int[]{0},
+                "desc",
+                prereqs,
+                postreqs,
+                false,
+                new Hours(1, false, 1, false, 1, false),
+                new Others(12, "dr. prof")
+        );
+
+        // @BenUBC let me know how we want this to not happen
+        // either a) don't allow there to be negative ids
+        // or b) remove initID
+        // or c) change the equality test to be not just id == id
+        // TODO
+        while (a.id() != 10)
+            a.initId();
+        while (b.id() != 10)
+            b.initId();
+
+        assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void equalsTest(){
+
+        Set<String> prereqs = new HashSet<>();
+        prereqs.add("pre1");
+        prereqs.add("pre2");
+
+        Set<String> postreqs = new HashSet<>();
+        postreqs.add("post1");
+        postreqs.add("post2");
+
+        Course a = new Course(
+                "code",
+                "name",
+                new int[]{0},
+                "desc",
+                prereqs,
+                postreqs,
+                false,
+                new Hours(1, false, 1, false, 1, false),
+                new Others(12, "dr. prof")
+        );
+
+        assertEquals(a, a);
+    }
+
+    @Test
+    public void immutabilityTests(){
+
+        Set<String> prereqs = new HashSet<>();
+        prereqs.add("pre1");
+        prereqs.add("pre2");
+
+        Set<String> postreqs = new HashSet<>();
+        postreqs.add("post1");
+        postreqs.add("post2");
+
+        int[] credits = new int[] {0};
+
+        Hours h = new Hours(1, false, 1, false, 1, false);
+        Others o = new Others(12, "dr. prof");
+
+        Course a = new Course(
+                "code",
+                "name",
+                credits,
+                "desc",
+                prereqs,
+                postreqs,
+                false,
+                h,
+                o
+        );
+
+        // credits
+        int[] creditCopy = a.getCredits();
+        creditCopy[0] = 100;
+        assertNotEquals(creditCopy[0], a.getCredits()[0]);
+        assertEquals(credits[0], a.getCredits()[0]);
+        credits[0] = 100;
+        assertNotEquals(credits[0], a.getCredits()[0]);
+
+        // pre req
+        Set<String> copiedPreqs = a.getPreRequisites();
+        copiedPreqs.add("EVIL VALUD");
+        assertNotEquals(copiedPreqs, a.getPreRequisites());
+        assertEquals(prereqs, a.getPreRequisites());
+
+        prereqs.add("EVIL");
+        assertNotEquals(prereqs, a.getPreRequisites());
+
+        // post req
+        Set<String> copiedPostreqs = a.getPostRequisites();
+        copiedPostreqs.add("EVIL VALUD");
+        assertNotEquals(copiedPostreqs, a.getPostRequisites());
+        assertEquals(prereqs, a.getPostRequisites());
+
+        postreqs.add("EVIL");
+        assertNotEquals(postreqs, a.getPostRequisites());
+
+        // weekly hours
+        assertEquals(h, a.getWeeklyHours());
+        Hours alsoH =  new Hours(1, false, 1, false, 1, false);
+        assertEquals(alsoH, h);
+        assertEquals(alsoH, a.getWeeklyHours());
+
+        // other
+        assertEquals(o, a.getOthers());
+        Others alsoO = new Others(12, "dr. prof");
+        assertEquals(alsoO, o);
+        assertEquals(alsoO, a.getOthers());
+    }
+
+
 }
