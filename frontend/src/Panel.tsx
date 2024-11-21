237ad345887e47cb1886ddc4094d3bd670978c9e
Iain05 <igriesdale@gmail.com>
2024-11-21 12:30:41 -0800

    added gradient lol

--- frontend/src/Panel.tsx
+++ frontend/src/Panel.tsx
@@ -7,8 +7,8 @@ export default function Panel(props: { currentCourse: Course | undefined }) {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 lg:top-20 lg:left-0 lg:right-auto h-1/4 lg:h-auto lg:w-1/4 m-5 bg-white shadow-xl rounded-2xl p-3 overflow-y-auto overflow-x-wrap rounded-box">
-      <h1 className="text-xl font-semibold ubc-blue">{props.currentCourse.code} - {props.currentCourse.name}</h1>
+    <div className="fixed bottom-0 left-0 right-0 lg:top-20 lg:left-0 lg:right-auto h-1/4 lg:h-auto lg:w-1/4 m-5 bg-white shadow-xl rounded-2xl p-3 overflow-y-auto overflow-x-wrap rounded-box-gradient">
+      <h1 className="text-xl course-title">{props.currentCourse.code.replace('-', ' ')} - {props.currentCourse.name}</h1>
       <p className="text-gray-500 my-1">
         {props.currentCourse.credits.length > 0 ? "Credits: " + props.currentCourse.credits.join("-") : ""} &nbsp;
         {props.currentCourse.schedule.lectures != -1 ?
--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { API_ENDPOINT } from "./Course";
 import Fuse from "fuse.js";
 
-export default function Search(props: {entered: (a: string)=>void}) {
+export default function Search(props: { entered: (a: string) => void }) {
 
   const [text, setText] = useState("");
 
@@ -12,7 +12,7 @@ export default function Search(props: {entered: (a: string)=>void}) {
   const [selected, setSelected] = useState("");
 
   const fuseRef = useRef<Fuse<string> | null>(null);
-  const [searchResults, setSearchResults] = useState<{name: string, code: string}[]>([]);
+  const [searchResults, setSearchResults] = useState<{ name: string, code: string }[]>([]);
 
   useEffect(() => {
     fetch(API_ENDPOINT + "/api/getallcourses").then(async (response) => {
@@ -23,14 +23,14 @@ export default function Search(props: {entered: (a: string)=>void}) {
       const json = await response.json();
 
       // TODO: Perhaps we should do some validation here...
-      let output = json.map((a: string) => {return a.replace("-", " ")});
+      let output = json.map((a: string) => { return a.replace("-", " ") });
       setCourses(output);
 
     });
   }, []);
 
   useEffect(() => {
-      fuseRef.current = new Fuse<string>(courses, {includeScore: false});
+    fuseRef.current = new Fuse<string>(courses, { includeScore: false });
   }, [courses]);
 
 
@@ -38,7 +38,7 @@ export default function Search(props: {entered: (a: string)=>void}) {
     if (fuseRef.current !== null) {
       let results = fuseRef.current.search(text);
       setSearchResults(results.map((res) => {
-        return {name: res.item, code: res.item.replace(" ", "-")}
+        return { name: res.item, code: res.item.replace(" ", "-") }
       }).filter((_v, idx) => idx < 5));
     }
   }, [text]);
@@ -70,7 +70,7 @@ export default function Search(props: {entered: (a: string)=>void}) {
         idx = (idx + 1) % searchResults.length;
         setSelected(searchResults[idx].code);
       }
-      
+
       e.preventDefault();
     }
     else if (key == "ArrowUp" || (key == "Tab" && shiftKey)) {
@@ -90,40 +90,40 @@ export default function Search(props: {entered: (a: string)=>void}) {
 
   // {/*onBlur={()=>{setFocus(false)}} onFocus={()=>{setFocus(true)}}*/}
   return (
-    <div className="fixed top-14 lg:top-0 left-0 right-0 m-5 bg-white shadow-md rounded-xl p-3 lg:w-1/4 rounded-box"
-        onFocus={() => {setFocus(true)}} onBlur={() => {setTimeout(() => {setFocus(false)}, 100)}}>
+    <div className="fixed top-16 lg:top-0 left-0 right-0 m-5 bg-white shadow-lg rounded-xl p-3 lg:w-1/4 rounded-box-gradient"
+      onFocus={() => { setFocus(true) }} onBlur={() => { setTimeout(() => { setFocus(false) }, 100) }}>
       <div className="w-full flex flex-col" >
         <div className="w-full flex flex-row">
           <input className="appearance-none bg-transparent border-gray-60 border-b-2 flex flex-grow text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-            onChange={(e) => {setText(e.target.value)}} value={text} 
-            placeholder="CPEN 211" 
-            onKeyDown={(e) => {keyPressed(e.key, e.shiftKey, e)}}
+            onChange={(e) => { setText(e.target.value) }} value={text}
+            placeholder="CPEN 211"
+            onKeyDown={(e) => { keyPressed(e.key, e.shiftKey, e) }}
           />
           <button className="lg:hidden flex flex-shrink py-2">
-            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
           </button>
         </div>
         {focus ? <div className="flex w-full flex-col">
           {
-            searchResults.map((res) => 
+            searchResults.map((res) =>
               <button key={res.code} className={`p-3 border-b-2 border-b-gray-50 ${selected == res.code ? "bg-gray-100" : ""}`}
-                  onPointerDown={() => {
-                    setText(res.name);
-                    setFocus(false);
-                    props.entered(res.code);
-                  }}
-                  onMouseEnter={() => {
-                    setSelected(res.code);
-                  }}
-                  onMouseLeave={() => {
-                    setSelected("");
-                  }}
+                onPointerDown={() => {
+                  setText(res.name);
+                  setFocus(false);
+                  props.entered(res.code);
+                }}
+                onMouseEnter={() => {
+                  setSelected(res.code);
+                }}
+                onMouseLeave={() => {
+                  setSelected("");
+                }}
               >
                 {res.name}
               </button>
             )
           }
-        </div>: <span></span>}
+        </div> : <span></span>}
       </div>
     </div>
   )
