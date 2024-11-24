a4fa8988ddecaef3f44c226a021f351575d727c3
William Banquier <54950146+William-Banquier@users.noreply.github.com>
2024-11-24 02:20:08 -0800

    Update JacocoGeneratedExclude.java

--- app/src/main/java/org/util/JacocoGeneratedExclude.java
+++ app/src/main/java/org/util/JacocoGeneratedExclude.java
@@ -2,8 +2,15 @@ package org.util;
 
 import java.lang.annotation.*;
 
+/**
+ * Annotation for exclusion from Jacoco, to be used on main methods and stuff
+ * We can exclude files in the gradle.build but this is helpful for methods
+ * <a href="https://www.baeldung.com/jacoco-report-exclude">[Source]</a>
+ *
+ * @author William Banquier
+ * @author baeldung.com
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
-public @interface JacocoGeneratedExclude {
-}
+public @interface JacocoGeneratedExclude { }
